5936e3d248e656efa2b9dc8bad130cab90abda9f
Leandro M Barbosa <lbarbosa@linux.com>
2014-08-13 17:03:46 -0300

    Change to zenburn theme

--- init-themes.el
+++ init-themes.el
@@ -3,6 +3,7 @@
 
 (require-package 'color-theme-sanityinc-solarized)
 (require-package 'color-theme-sanityinc-tomorrow)
+(require-package 'zenburn-theme)
 
 ;;------------------------------------------------------------------------------
 ;; Old-style color theming support (via color-theme.el)
