5eb3e77af321fd6853d5dadf4c17b1b0089bf855
Leandro M. Barbosa <lbarbosa@linux.com>
2016-03-18 09:31:43 -0300

    Use company-jedi vs jedi

--- lisp/init-jedi.el
+++ lisp/init-jedi.el
@@ -1,6 +1,10 @@
-(require-package 'jedi)
-(global-auto-complete-mode t)
-(add-hook 'python-mode-hook 'jedi:setup)
-(setq jedi:setup-keys t)                      ; optional
-(setq jedi:complete-on-dot t)                 ; optional)
+(require-package 'company-jedi)
+                                        ;(global-auto-complete-mode t)
+                                        ;(add-hook 'python-mode-hook 'jedi:setup)
+                                        ;(setq jedi:setup-keys t)                      ; optional
+                                        ;(setq jedi:complete-on-dot t)                 ; optional)
+(defun my/python-mode-hook ()
+  (add-to-list 'company-backends 'company-jedi))
+
+(add-hook 'python-mode-hook 'my/python-mode-hook)
 (provide 'init-jedi)
