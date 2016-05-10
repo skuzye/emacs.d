28c2de36ab219bb77ea1d90be3902ce68332b98b
Leandro M. Barbosa <lbarbosa@linux.com>
2016-05-10 13:01:40 -0300

    Fix jedi

--- lisp/init-jedi.el
+++ lisp/init-jedi.el
@@ -4,7 +4,9 @@
                                         ;(setq jedi:setup-keys t)                      ; optional
                                         ;(setq jedi:complete-on-dot t)                 ; optional)
 (defun my/python-mode-hook ()
-  (add-to-list 'company-backends 'company-jedi))
+  (add-to-list 'company-backends 'company-jedi)
+  (jedi:setup)
+  (setq jedi:complete-on-dot t))
 
 (add-hook 'python-mode-hook 'my/python-mode-hook)
 (provide 'init-jedi)
