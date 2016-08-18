6c3a6984d1131dc5b4cacd0e21351e9bbcfabc07
Leandro M. Barbosa <lbarbosa@linux.com>
2016-08-18 13:36:03 -0300

    Try to fix Go integration

--- lisp/init-go.el
+++ lisp/init-go.el
@@ -1,4 +1,22 @@
-(add-hook 'before-save-hook #'gofmt-before-save)
+(require-package 'go-mode)
+(require-package 'company-go)
+
+(setenv "PATH"
+        (concat
+         (getenv "HOME")
+         "/repos/go/bin" ":"
+         (getenv "PATH")
+         )
+        )
+
+(add-hook 'go-mode-hook(lambda ()
+                         ))
+
+(add-hook 'go-mode-hook(lambda ()
+                         (add-hook 'before-save-hook #'gofmt-before-save nil 'local)
+                         (set (make-local-variable 'company-backends) '(company-go))
+                         (local-set-key (kbd "C-c .") 'godef-jump)
+                         (local-set-key (kbd "C-c ,") 'pop-tag-mark)
+                         (company-mode)))
 
 (provide 'init-go)
-
